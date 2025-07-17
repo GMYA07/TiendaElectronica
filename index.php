2aabf67e44d388162e53d3e4a23b8e40da5e94f1
Yael Matamoros <yaelguzman750@gmail.com>
2025-07-16 20:25:09 -0600

    se realizo el menu

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Electronics</title>
+    <?php include './php/enlaces/enlaces.php'; ?>
+</head>
+<body>
+    <?php include './views/cabecera/cabecera.php'; ?>
+</body>
+
+</html>
---
+++ views/cabecera/cabecera.php
@@ -0,0 +1,51 @@
+<nav class="navbar navbar-expand-lg ">
+    <div class="container-fluid contenedorMenu">
+        <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand contenedorLogo" href="#">
+            <img class="logoNav" src="./Assets/img/logoEmpresa.png" alt="">
+        </a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="container contenedorListaItems">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 itemsMenu">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Categorias
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Computo y Tablets</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Mundo Gaming</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Promociones</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Celulares</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Gaming</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Computacion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Audio y video</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="container contenedorBotonesAcciones">
+                <a href="#"><i class="fa fa-user" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+</nav>
