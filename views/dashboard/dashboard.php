7aef6002dfc284929fc20d996f2e5017978d5d8f
GMYA07 <yaelguzman750@gmail.com>
2025-09-26 09:44:02 -0600

    empezando diseno de dashboard

--- views/dashboard/dashboard.php
+++ views/dashboard/dashboard.php
@@ -76,18 +76,28 @@
 <div class="container">
     <div class="row">
         <div class="col-8">
-            <div class="col-6 d-flex justify-content-center">
-                <div class="iconoDisenoTargeta text-center ">
-                    <i class="fa fa-user" aria-hidden="true"></i>
-                </div>
-            </div>
-            <div class="chart-container" style="position: relative; height:40vh; width:80vw">
+            <div class="chart-container" style="position: relative; height:50vh; width:100vh;">
                 <canvas id="chart2"></canvas>
             </div>
         </div>
         <div class="col-4">
             <div>
                 <canvas id="myChart"></canvas>
+            </div>
+            <div class="container targetasTop p-2">
+                <div class="container-fluid pt-4">
+                    <p class="tituloTargeta">Clientes Nuevos</p>
+                </div>
+                <div class="row px-3 mt-0">
+                    <div class="col-6 d-flex flex-column">
+                        <p class="informacionTargeta">26</p>
+                    </div>
+                    <div class="col-6 d-flex justify-content-center">
+                        <div class="iconoDisenoTargeta text-center ">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -115,17 +125,48 @@
         }
     });
 
-    const chart = new Chart(ct2, {
-        type: 'line',
-        data: data,
-        options: {
-            onClick: (e) => {
-                const canvasPosition = getRelativePosition(e, chart);
+    (async function() {
+        const data = [{
+                year: 2010,
+                count: 10
+            },
+            {
+                year: 2011,
+                count: 20
+            },
+            {
+                year: 2012,
+                count: 15
+            },
+            {
+                year: 2013,
+                count: 25
+            },
+            {
+                year: 2014,
+                count: 22
+            },
+            {
+                year: 2015,
+                count: 30
+            },
+            {
+                year: 2016,
+                count: 28
+            },
+        ];
 
-                // Substitute the appropriate scale IDs
-                const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
-                const dataY = chart.scales.y.getValueForPixel(canvasPosition.y);
+        new Chart(
+            document.getElementById('chart2'), {
+                type: 'line',
+                data: {
+                    labels: data.map(row => row.year),
+                    datasets: [{
+                        label: 'Acquisitions by year',
+                        data: data.map(row => row.count)
+                    }]
+                }
             }
-        }
-    });
+        );
+    })();
 </script>
